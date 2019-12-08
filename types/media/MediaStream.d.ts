f916d047f67a60915e35eecfd0d1ddcaf199c21c
gunnermadsen <gunner.madsen@outlook.com>
2019-12-07 18:03:55 -0800

    testing initial deployment using separate webrtc definition file

--- types/media/MediaStream.d.ts
+++
@@ -1,7 +0,0 @@
-interface MediaDevices {
-    getSupportedConstraints(): MediaTrackSupportedConstraints;
-
-    getUserMedia(constraints: MediaStreamConstraints): Promise<MediaStream>;
-    getDisplayMedia(constraints: MediaStreamConstraints): Promise<MediaStream>;
-    enumerateDevices(): Promise<MediaDeviceInfo[]>;
-}
